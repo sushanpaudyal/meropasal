76ececb3a64b0e77f476f9f4c3f1a80b374b5faf
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-01-21 18:42:49 +0545

    Showing Products in Front page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class IndexController extends Controller
 {
     public function index(){
-        return view ('frontend.index');
+        $products = Product::latest()->get();
+        return view ('frontend.index',compact('products'));
     }
 }
