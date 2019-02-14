3385b3e6a8bfd21b3f4551cea8935ab70b65e625
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-02-14 18:24:51 +0545

    valentine day extra class

--- resources/views/frontend/products/cart.blade.php
+++ resources/views/frontend/products/cart.blade.php
@@ -29,7 +29,7 @@
                     </tr>
                     </thead>
                     <tbody>
-
+                    <?php $total_amount = 0; ?>
                     @foreach($userCart as $cart)
                     <tr>
                         <td class="cart_product">
@@ -58,6 +58,8 @@
                             <a class="cart_quantity_delete" href="{{route('delete.cart', $cart->id)}}"><i class="fa fa-times"></i></a>
                         </td>
                     </tr>
+
+                        <?php $total_amount = $total_amount + ($cart->price * $cart->quantity); ?>
                         @endforeach
 
 
@@ -76,54 +78,8 @@
             <div class="row">
                 <div class="col-sm-6">
                     <div class="chose_area">
-                        <ul class="user_option">
-                            <li>
-                                <input type="checkbox">
-                                <label>Use Coupon Code</label>
-                            </li>
-                            <li>
-                                <input type="checkbox">
-                                <label>Use Gift Voucher</label>
-                            </li>
-                            <li>
-                                <input type="checkbox">
-                                <label>Estimate Shipping & Taxes</label>
-                            </li>
-                        </ul>
-                        <ul class="user_info">
-                            <li class="single_field">
-                                <label>Country:</label>
-                                <select>
-                                    <option>United States</option>
-                                    <option>Bangladesh</option>
-                                    <option>UK</option>
-                                    <option>India</option>
-                                    <option>Pakistan</option>
-                                    <option>Ucrane</option>
-                                    <option>Canada</option>
-                                    <option>Dubai</option>
-                                </select>
 
-                            </li>
-                            <li class="single_field">
-                                <label>Region / State:</label>
-                                <select>
-                                    <option>Select</option>
-                                    <option>Dhaka</option>
-                                    <option>London</option>
-                                    <option>Dillih</option>
-                                    <option>Lahore</option>
-                                    <option>Alaska</option>
-                                    <option>Canada</option>
-                                    <option>Dubai</option>
-                                </select>
 
-                            </li>
-                            <li class="single_field zip-field">
-                                <label>Zip Code:</label>
-                                <input type="text">
-                            </li>
-                        </ul>
                         <a class="btn btn-default update" href="">Get Quotes</a>
                         <a class="btn btn-default check_out" href="">Continue</a>
                     </div>
@@ -131,10 +87,7 @@
                 <div class="col-sm-6">
                     <div class="total_area">
                         <ul>
-                            <li>Cart Sub Total <span>$59</span></li>
-                            <li>Eco Tax <span>$2</span></li>
-                            <li>Shipping Cost <span>Free</span></li>
-                            <li>Total <span>$61</span></li>
+                            <li>Total <span>Rs. <?php echo $total_amount; ?></span></li>
                         </ul>
                         <a class="btn btn-default update" href="">Update</a>
                         <a class="btn btn-default check_out" href="">Check Out</a>
