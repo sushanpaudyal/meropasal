a54df56a95c65dc8a4d1e17b3354a051364bc959
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-02-14 18:49:09 +0545

    Saving coupns

--- app/Http/Controllers/CouponsController.php
+++ app/Http/Controllers/CouponsController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Coupon;
 use Illuminate\Http\Request;
+use Session;
 
 class CouponsController extends Controller
 {
     public function addCoupon(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->all();
+            $coupon = new Coupon;
+            $coupon->coupon_code = strtoupper($data['coupon_code']);
+            $coupon->amount = $data['amount'];
+            $coupon->amount_type = $data['amount_type'];
+            $coupon->expiry_date = $data['expiry_date'];
+
+            if(empty($data['status'])){
+                $coupon->status = 0;
+            } else {
+                $coupon->status = 1;
+            }
+            $coupon->save();
+            Session::flash('success', 'Coupon Created');
+            return redirect()->back();
+        }
         return view ('admin.products.add_coupon');
     }
 }
